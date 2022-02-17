721c0060bb18ffbfa093a800353e369e08572e85
Koushik Kiran Kumar <54726324+ornkkk@users.noreply.github.com>
2022-02-17 17:54:58 +0000

    Configured Admin model displays

--- reviews/admin.py
+++ reviews/admin.py
@@ -2,12 +2,34 @@ from django.contrib import admin
 
 from reviews.models import Publisher, Contributor, Book, BookContributor, Review
 
+class BookAdmin(admin.ModelAdmin):
+    date_hierarchy = 'publication_date'
+    list_display = ('title', 'isbn13')
+    list_filter = ('publisher', 'publication_date')
+    search_fields = ('title', 'isbn__exact', 'publisher__name__startswith')
+
+def initialled_name(obj):
+    """
+  obj.first_names='Jerome David', obj.last_names='Salinger' => 'Salinger, JD'
+  """
+    initials = '. '.join([name[0] for name in obj.first_names.split(" ")])+'.'
+    return f"{obj.last_names}, {initials}"
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = (initialled_name,)
+
+class ReviewAdmin(admin.ModelAdmin):
+    #exclude = ('date_edited')
+    #fields = ('content', 'rating', 'creator', 'book')
+    #fieldsets = (('Linkage', {'fields': ('creator', 'book')}), ('Review content', {'fields': ('content', 'rating')}))
+    fieldsets = ((None, {'fields': ('creator', 'book')}), ('Review content', {'fields': ('content', 'rating')}))
+
 
 # Register your models here.
 admin.site.register(Publisher)
-admin.site.register(Contributor)
-admin.site.register(Book)
+admin.site.register(Contributor, ContributorAdmin)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookContributor)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
 
 
--- reviews/models.py
+++ reviews/models.py
@@ -16,14 +16,18 @@ class Book(models.Model):
   """A published book."""
   title = models.CharField(max_length=70,
                            help_text="The title of the book.")
-  publication_date = models.DateField(verbose_name="Date the book was published.")
+  publication_date = models.DateField(verbose_name="Published Date")
   isbn = models.CharField(max_length=20,
-                          verbose_name="ISBN number of the book.")
+                          verbose_name="ISBN")
   publisher = models.ForeignKey(Publisher,
                                 on_delete=models.CASCADE, default=-1)
 
   def __str__(self):
-    return self.title
+    return f"{self.title} (ISBN: {self.isbn})"
+
+  def isbn13(self):
+      """ '9780316769174' => '978-0-31-676917-4' """
+      return f"{self.isbn[0:3]}-{self.isbn[3:4]}-{self.isbn[4:6]}-{self.isbn[6:12]}-{self.isbn[12:13]}"
 
 class Contributor(models.Model):
   """A contributor to a Book, e.g. Author, Editor, Co-Author."""
@@ -48,7 +52,7 @@ class BookContributor(models.Model):
                            on_delete=models.CASCADE)
   contributor = models.ForeignKey(Contributor,
                                   on_delete=models.CASCADE)
-  role = models.CharField(verbose_name="The role this contributor had in the book.",
+  role = models.CharField(verbose_name="Role",
                           choices=ContributionRole.choices,
                           max_length=20)
 
