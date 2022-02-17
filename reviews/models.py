b6e3e1fe109f086f6a7163ca0887ca3a721a7ab2
Koushik Kiran Kumar <54726324+ornkkk@users.noreply.github.com>
2022-02-17 05:29:30 +0000

    Updated and migrated models

--- reviews/models.py
+++ reviews/models.py
@@ -9,6 +9,9 @@ class Publisher(models.Model):
   website = models.URLField(help_text="The Publisher's website.")
   email = models.EmailField(help_text="The Publisher's email address.")
 
+  def __str__(self):
+    return self.name
+
 class Book(models.Model):
   """A published book."""
   title = models.CharField(max_length=70,
@@ -17,7 +20,10 @@ class Book(models.Model):
   isbn = models.CharField(max_length=20,
                           verbose_name="ISBN number of the book.")
   publisher = models.ForeignKey(Publisher,
-                                on_delete=models.CASCADE)
+                                on_delete=models.CASCADE, default=-1)
+
+  def __str__(self):
+    return self.title
 
 class Contributor(models.Model):
   """A contributor to a Book, e.g. Author, Editor, Co-Author."""
@@ -26,8 +32,11 @@ class Contributor(models.Model):
   last_names = models.CharField(max_length=50,
                                 help_text="The contributor's last name or names.")
   email = models.EmailField(help_text="The contact email of the contributor.")
-  contributors = models.MantToManyField('Contributor',
+  contributors = models.ManyToManyField('Contributor',
                                         through="BookContributor")
+
+  def __str__(self):
+    return self.first_names
 
 class BookContributor(models.Model):
   class ContributionRole(models.TextChoices):
@@ -39,12 +48,12 @@ class BookContributor(models.Model):
                            on_delete=models.CASCADE)
   contributor = models.ForeignKey(Contributor,
                                   on_delete=models.CASCADE)
-  roel = models.CharField(verbose_name="The role this contributor had in the book.",
+  role = models.CharField(verbose_name="The role this contributor had in the book.",
                           choices=ContributionRole.choices,
                           max_length=20)
 
 class Review(models.Model):
-  content = models.textField(help_text="The review text.")
+  content = models.TextField(help_text="The review text.")
   rating = models.IntegerField(help_text="The rating the reviewer has given.")
   date_created = models.DateTimeField(auto_now_add=True,
                                       help_text="The date and time the review was                                                     created.")
